b2910a1520d829420ee58831644d0e744f20cb17
Timothy <jemyoctavian@gmail.com>
2025-08-22 09:10:56 +0700

    de

--- startup.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Menambahkan registry untuk startup StorageMonitor.exe di folder C:\Program Files\Storage
-
-set "appPath=C:\Program Files\Storage\StorageMonitor.exe"
-
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v StorageMonitor /t REG_SZ /d "\"%appPath%\"" /f
-
-echo Registry startup StorageMonitor sudah ditambahkan.
-pause
